5254cbbdba537e4c40773a43fce1c03c560a6727
vijay-dev-x <vm2218943@gmail.com>
2024-11-10 20:20:30 +0530

    changed UI color of  login page

--- src/pages/user/UserLogin.jsx
+++ src/pages/user/UserLogin.jsx
@@ -364,10 +364,20 @@ const UserLogin = () => {
                     animate={{ y: 0, opacity: 1 }}
                     transition={{ delay: 0.2 }}
                   >
+                    <a
+                      href="/user/dashboard"
+                      className=" flex flex-col justify-center mb-5 items-center mr-4"
+                    >
+                      <img
+                        src="/local-logo.png"
+                        alt="Forex Funding Logo"
+                        className=" object-contain w-auto h-10 md:h-16 sm:h-8"
+                      />
+                    </a>
                     <h2 className="text-4xl font-bold mb-2 text-white">
                       Welcome back
                     </h2>
-                    <p className="text-secondary-300 mb-8">
+                    <p className="text-secondary-500 mb-8">
                       Enter your credentials to access your account
                     </p>
                   </motion.div>
@@ -379,7 +389,7 @@ const UserLogin = () => {
                       className="relative"
                     >
                       <Mail
-                        className="absolute top-3 left-3 text-secondary-400"
+                        className="absolute top-3 left-3 text-secondary-500"
                         size={20}
                       />
                       <input
@@ -399,7 +409,7 @@ const UserLogin = () => {
                       className="relative"
                     >
                       <Lock
-                        className="absolute top-3 left-3 text-secondary-400"
+                        className="absolute top-3 left-3 text-secondary-500"
                         size={20}
                       />
                       <input
@@ -416,7 +426,7 @@ const UserLogin = () => {
                       <button
                         type="button"
                         onClick={() => setShowForgotPassword(true)}
-                        className="text-sm text-green-500 hover:text-white hover:  transition-all"
+                        className="text-sm text-secondary-500 hover:text-white hover:  transition-all"
                       >
                         Forgot password?
                       </button>
@@ -426,7 +436,7 @@ const UserLogin = () => {
                       animate={{ y: 0, opacity: 1 }}
                       transition={{ delay: 0.5 }}
                       type="submit"
-                      className="w-full flex gap-2 items-center group justify-center py-3 px-4 border border-transparent rounded-lg text-white bg-green-600 hover:bg-green-600/80 focus:outline-none focus:ring-1 focus:ring-offset-2 focus:ring-green-500 transition transform hover:scale-105"
+                      className="w-full flex gap-2 items-center group justify-center py-3 px-4 border border-transparent rounded-lg text-white bg-blue-500 hover:bg-blue-500/80 focus:outline-none focus:ring-1 focus:ring-offset-2 focus:ring-green-500 transition transform hover:scale-105"
                     >
                       <LogIn className="mr-1" size={20} />
                       <p className="group-hover:animate-pulse transition-all">
@@ -442,11 +452,11 @@ const UserLogin = () => {
                   transition={{ delay: 0.6 }}
                   className="px-8 py-4 bg-secondary-700/30 bg-opacity-50 border-t border-secondary-600"
                 >
-                  <p className="text-center text-sm text-secondary-300">
+                  <p className="text-center text-sm text-secondary-500/70">
                     Don't have an account?{" "}
                     <Link
                       to="/user/signup"
-                      className="font-medium text-green-500 hover:text-green-400 transition"
+                      className="font-medium text-secondary-500 hover:text-secondary-500/80 transition"
                     >
                       Sign up
                     </Link>
