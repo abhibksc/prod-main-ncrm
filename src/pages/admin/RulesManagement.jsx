bd7029cb27b58484264d701ff202953b588a7036
vijay-dev-x <vm2218943@gmail.com>
2024-11-24 18:02:26 +0530

    changed focous color

--- src/pages/admin/RulesManagement.jsx
+++ src/pages/admin/RulesManagement.jsx
@@ -224,7 +224,7 @@ const RulesManagement = () => {
                               text: e.target.value,
                             })
                           }
-                          className="w-full px-3 py-2 rounded-lg bg-gray-700/30 text-gray-100 border border-gray-600/40 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent resize-none"
+                          className="w-full px-3 py-2 rounded-lg bg-gray-700/30 text-gray-100 border border-gray-600/40 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:border-transparent resize-none"
                         />
                       </div>
                       <div className="flex items-center gap-3">
