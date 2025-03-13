117a6ecf8f144a0a8b90e3a3c29cfcad90fe44b8
pixel <vm2218943@gmail.com>
2025-03-13 18:12:57 +0530

    added 20

--- src/components/user/UserSidebar.jsx
+++ src/components/user/UserSidebar.jsx
@@ -172,7 +172,7 @@ const UserSidebar = () => {
               <img
                 src={siteConfig?.logo}
                 alt="Forex Logo"
-                className=" object-contain w-auto h-10 md:h-80 sm:h-10"
+                className=" object-contain w-auto h-10 md:h-20 sm:h-10"
               />
             </a>
           </div>
