ead796da79db65e4b894f4a58830ba386f114160
pixel <vm2218943@gmail.com>
2025-05-09 11:37:05 +0530

    added 2025

--- src/components/emails/WithdrwalsMails.jsx
+++ src/components/emails/WithdrwalsMails.jsx
@@ -125,7 +125,7 @@ const withdrawRequestMail = ({ loggedUser, amount, accountBalance }) => {
     import.meta.env.VITE_EMAIL_EMAIL
   }</a></p>
             <p>We sent out this message to all existing traders. Please visit this page to know more about our Privacy Policy.</p>
-            <p>&copy; 2024 ${
+            <p>&copy; 2025 ${
               import.meta.env.VITE_WEBSITE_NAME
             }. All Rights Reserved</p>
           </div>
--- src/pages/user/UserVerify.jsx
+++ src/pages/user/UserVerify.jsx
@@ -202,7 +202,7 @@ const UserVerify = () => {
     import.meta.env.VITE_EMAIL_EMAIL
   }</a></p>
             <p>We sent out this message to all existing traders. Please visit this page to know more about our Privacy Policy.</p>
-            <p>&copy; 2024 ${
+            <p>&copy; 2025 ${
               import.meta.env.VITE_WEBSITE_NAME
             }. All Rights Reserved</p>
           </div>
