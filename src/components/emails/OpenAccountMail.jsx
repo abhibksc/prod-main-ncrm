b7c11e843dc3c4c4bfcb9f3c525e5f35f6205e36
pixel <vm2218943@gmail.com>
2025-06-05 13:00:29 +0530

    removed address

--- src/components/emails/OpenAccountMail.jsx
+++ src/components/emails/OpenAccountMail.jsx
@@ -131,20 +131,19 @@ export default function OpenAccountMail({
 
               </div>
                <div class="footer">
-                <div class="footer-info">
-           <p>${import.meta.env.VITE_EMAIL_ADDRESS || "forextest@mail.com"}</p>
+                 <div class="footer-info">
                   <p>Website: <a href="https://${
                     import.meta.env.VITE_EMAIL_WEBSITE
                   }"> ${
     import.meta.env.VITE_EMAIL_WEBSITE
-  } </a> | E-mail: <a href="mailto:${
-    import.meta.env.VITE_EMAIL_EMAIL || "forextest@mail.com"
-  }">${import.meta.env.VITE_EMAIL_EMAIL || "forextest@mail.com"}</a></p>
+  } </a> | E-mail: <a href="mailto:${import.meta.env.VITE_EMAIL_EMAIL || ""}">${
+    import.meta.env.VITE_EMAIL_EMAIL || ""
+  }</a></p>
                   <p>We sent out this message to all existing ${
-                    import.meta.env.VITE_WEBSITE_NAME || "Forex"
+                    import.meta.env.VITE_WEBSITE_NAME || ""
                   } traders. Please visit this page to know more about our Privacy Policy.</p>
                   <p>© 2025 ${
-                    import.meta.env.VITE_WEBSITE_NAME || "Forex"
+                    import.meta.env.VITE_WEBSITE_NAME || ""
                   }. All Rights Reserved</p>
                 </div>
               </div>
