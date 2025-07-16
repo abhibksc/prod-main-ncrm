9c981ce400e02983a889af191506c1bb8949ad42
pixel <vm2218943@gmail.com>
2025-07-16 10:33:03 +0530

    added cash-withdrawal

--- src/pages/user/UserWithdraw.jsx
+++ src/pages/user/UserWithdraw.jsx
@@ -255,6 +255,12 @@ const UserWithdraw = () => {
                   >
                     Wallet Transfer
                   </option>
+                  <option
+                    className=" bg-secondary-800 text-white"
+                    value="Cash Withdrawal"
+                  >
+                    Cash Withdrawal
+                  </option>
                 </select>
               </div>
 
--- src/pages/user/referral/UserReferalWithdrwal.jsx
+++ src/pages/user/referral/UserReferalWithdrwal.jsx
@@ -276,6 +276,11 @@ export const UserReferralWithdrawal = () => {
                 {/* <option value="">Select Gateway</option> */}
                 <option value="Bank Transfer">Bank Transfer</option>
                 <option value="Wallet Transfer">Wallet Transfer</option>
+                <option
+                    value="Cash Withdrawal"
+                  >
+                    Cash Withdrawal
+                  </option>
               </select>
             </div>
 
