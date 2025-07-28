a70413db53573a93ab2e3e5eb4a627d821070e22
pixel <vm2218943@gmail.com>
2025-07-28 12:28:06 +0530

    aaded copy requests

--- src/components/admin/SideBar.jsx
+++ src/components/admin/SideBar.jsx
@@ -151,11 +151,11 @@ const Sidebar = () => {
         { label: "All IB Withdrawals", route: "/admin/ib-withdrawal/all" },
       ],
     },
-    // {
-    //   label: "Copy Requests",
-    //   icon: <CopyPlusIcon />,
-    //   route: "/admin/copy-requests",
-    // },
+    {
+      label: "Copy Requests",
+      icon: <CopyPlusIcon />,
+      route: "/admin/copy-requests",
+    },
     {
       label: "Payment Getaways",
       icon: <CreditCard />,
